674a84696c84b9ebfb93e9b7ab72807a9c88c60d
Damian Czarnota <godevdamian@gmail.com>
2021-01-30 13:28:23 +0100

    init global var

--- constants.ts
+++ constants.ts
@@ -1,3 +1,1 @@
-export const API_KEY = "api_key=00eb2c7f6fdd7eaea281bdd2675f076d";
-export const BASE_URL = "https://api.themoviedb.org/3/";
-export const BASE_PHOTO_URL = "https://image.tmdb.org/t/p/original";
+export const BASE_API_URL = "https://api.themoviedb.org/3/";
