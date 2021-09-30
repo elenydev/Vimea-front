c568ae58fc93f7509db9cdca34155fe2224bac23
damian <godevdamian@gmail.com>
2021-09-30 14:48:11 +0200

    Removed lowercase parse on firstName/lastName

--- repositories/auth/auth.ts
+++ repositories/auth/auth.ts
@@ -18,8 +18,8 @@ export const handleRegistration = async (
 ): Promise<AuthorizationRequestResult> => {
   const { firstName, lastName, email, password, avatar, policy } = user;
   const newUser = new FormData();
-  newUser.append("firstName", firstName.toLowerCase());
-  newUser.append("lastName", lastName.toLowerCase());
+  newUser.append("firstName", firstName);
+  newUser.append("lastName", lastName);
   newUser.append("email", email);
   newUser.append("password", password);
   newUser.append("avatar", avatar[0]);
