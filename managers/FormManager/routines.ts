3c6876b0e98a2454ed8d70c7fb19a088b4ad2c43
damian <godevdamian@gmail.com>
2021-10-01 13:42:26 +0200

    Routines migrations p2

---
+++ managers/FormManager/routines.ts
@@ -0,0 +1,11 @@
+import { createRoutine } from "utils/redux/routines";
+
+import FormManager from "managers/FormManager/FormManager";
+
+export enum FormManagerRoutines {
+  SetFormManager = "setFormManager",
+}
+
+export const setFormManager = createRoutine<FormManager, FormManager>(
+  FormManagerRoutines.SetFormManager
+);
